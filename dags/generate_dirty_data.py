16d68101f036f002ca28c6b14570c1391e312685
Gustavo Alencar <prosidegcr@gmail.com>
2026-04-11 18:41:23 -0300

    Correcting dag path in the airflow, and creating dags to generate the data in the landing zone

---
+++ dags/generate_dirty_data.py
@@ -0,0 +1,36 @@
+"""
+DAG to generate DIRTY synthetic data into the Landing Zone.
+"""
+import sys
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# Ensure src is in PYTHONPATH
+sys.path.append('/opt/airflow/src')
+
+# pylint: disable=wrong-import-position, import-error
+from utils.generate_dirty_data import generate_dirty_big_data
+
+DEFAULT_ARGS = {
+    'owner': 'analyst',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 29),
+    'retries': 0,
+}
+
+with DAG(
+    'generate_dirty_landing_data',
+    default_args=DEFAULT_ARGS,
+    description='Generates 1M rows with INTENTIONAL ANOMALIES in Landing Zone',
+    schedule_interval=None,
+    catchup=False,
+    tags=['utils', 'data_gen', 'dirty'],
+) as dag:
+
+    task_gen_dirty = PythonOperator(
+        task_id='generate_dirty_data',
+        python_callable=generate_dirty_big_data,
+    )
+
+    task_gen_dirty
